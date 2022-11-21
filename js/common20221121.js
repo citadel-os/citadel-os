8be94e32a28b36f99b919405a4e14ef7186a7158
jsays <josh@saysomething.com>
2022-11-21 13:05:53 -0600

    add wallet addr to check pilot, CITADEL-20

---
+++ js/common20221121.js
@@ -0,0 +1,122 @@
+import {web3, etherscan} from './contracts/addr20221121.js';
+
+//Global support functions
+export function showErrors(message) {
+  new_block();
+  block_log(`<span class='error'>${message}<span>`);
+}
+ 
+export function showSuccess(message, tx = false) {
+  new_block();
+  if (tx) {
+    block_log(`<span class='success'><a href="https://${etherscan}/tx/${tx}" target="_blank">${message}</a></span>`);
+  } else {
+    block_log(`<span class='success'>${message}<span>`);
+  }
+}
+
+export function smart_split(input, del, empty_space) {
+  if (input.length === 0) return input;
+  var outputs = [""];
+
+  var compare = function (base, insert, position) {
+    if (position + insert.length > base.length) return false;
+    for (var i = 0; i < insert.length; i++) {
+      if (!(base.charAt(position + i) === insert.charAt(i))) return false;
+    }
+    return true;
+  };
+
+  var quotes = false;
+  for (var i = 0; i < input.length; i++) {
+    var char = input.charAt(i);
+    if (char === '"') {
+      quotes = !quotes;
+      continue;
+    }
+
+    if (!quotes && compare(input, del, i)) {
+      outputs.push("");
+      i += del.length - 1;
+      continue;
+    }
+
+    outputs[outputs.length - 1] += char;
+  }
+
+  if (!empty_space) {
+    for (var i = 0; i < outputs.length; i++) {
+      if (outputs[i] === "") {
+        outputs.splice(i, 1);
+      }
+    }
+  }
+
+  return outputs;
+}
+
+export async function getAccounts() {
+//  await window.ethereum.enable();
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  updateConnectionStatus();
+  return accounts;
+}
+
+export async function getGasPrice() {
+  const price = await web3.eth.getGasPrice();
+  return price;
+}
+
+var current_block;
+
+export function new_block() {
+  var wrapper = document.getElementById("wrapper");
+  current_block = document.createElement("div");
+  current_block.classList.add("log");
+  wrapper.appendChild(current_block);
+}
+
+export function clear() {
+  var wrapper = document.getElementById("wrapper");
+  wrapper.innerHTML = "";
+}
+
+export function block_log(message) {
+  current_block.innerHTML += "<p>" + message + "</p>";
+  document.getElementById('screen').scrollTop = document.getElementById('screen').scrollHeight; //ensure input is visible
+  document.getElementById("input_source").focus(); //ensure input gets focus
+}
+
+export async function updateConnectionStatus() {
+  const walletDiv = document.getElementById("wallet_connection");
+  if (window.ethereum && ethereum.isConnected() && ethereum.selectedAddress) {
+    walletDiv.innerHTML = `--[${truncAddr(ethereum.selectedAddress,4)}]--`;
+    walletDiv.style.cursor = 'default';
+    walletDiv
+      .removeEventListener("click", function () {
+        ethereum.request({method:'eth_requestAccounts'}).then(updateConnectionStatus)
+      });
+  } else {
+    walletDiv.innerHTML = "<span style='color:red;'>--[disconnected]--</span>";
+    walletDiv.style.cursor = 'pointer';
+    walletDiv
+      .addEventListener("click", function () {
+        ethereum.request({method:'eth_requestAccounts'}).then(updateConnectionStatus)
+        .catch((error) => {
+          console.error(`Wallet connection failed: ${error.message}`);
+        })
+      });
+  }
+}
+
+export const ofInterest = ']08v';
+
+function truncAddr(addr, limit) {
+  if (addr.length <= (limit * 2)) {
+    return addr;
+  }
+  var shortAddr = `${addr.substr(0,limit)}...${addr.substr(limit * -1)}`
+  return shortAddr;
+}
+
+export const availKults = ['kult geheim','d0d engel','stalkroth','kult gor','klinge','dalk stracht','ys diaboli','grater djevel'];
---
+++ js/pilot20221121.js
@@ -0,0 +1,282 @@
+import * as spinner from './spinner20221121.js';
+import {availKults, showErrors, showSuccess, getAccounts, getGasPrice} from './common20221121.js';
+
+import {abiPilot} from './contracts/abi20221121.js';
+import {CITADEL_PILOT, web3, apiKey} from './contracts/addr20221121.js';
+
+//const CITADEL_PILOT = "";
+const citadelPilot = new web3.eth.Contract(abiPilot, CITADEL_PILOT);
+
+export async function claimPilot(citadel) {
+  var gasPrice = await getGasPrice();
+  gasPrice = Math.trunc(gasPrice * 1.5);
+
+  const accounts = await getAccounts();
+  if(accounts.length <= 0) {
+      throw new Error("connect to metamask");
+  }
+
+  const estimatedGas = await citadelPilot.methods.claim(citadel).estimateGas({from: accounts[0]});
+
+  const tx = {
+      'from': accounts[0],
+      'to': CITADEL_PILOT,
+      'data': citadelPilot.methods.claim(citadel).encodeABI(),
+      'gas': web3.utils.toHex(estimatedGas),
+      'gasPrice': web3.utils.toHex(gasPrice)
+  };
+
+  spinner.startSpinner();
+  const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [tx],
+  })
+  .then(
+    result => {
+      showSuccess('claim pilot tx complete',result)
+    },
+    error => {
+      showErrors(error.message)
+    }).
+  finally(() => {spinner.stopSpinner()});
+
+  return txHash;
+}
+
+export async function mintPilot(pilots) {
+  const accounts = await getAccounts();
+  if(accounts.length <= 0) {
+    throw new Error("connect to metamask");
+  }
+  
+  const mintAmt = (.125 * pilots).toString();
+  let ethWei = web3.utils.toWei(mintAmt, 'ether');
+  ethWei = web3.utils.toHex(ethWei);
+
+  const tx = {
+    'from': accounts[0],
+    'to': CITADEL_PILOT,
+    'value': ethWei,
+    'data': citadelPilot.methods.mintPilot(pilots).encodeABI()
+  };
+  
+  spinner.startSpinner();
+  const txHash = await window.ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [tx],
+  })
+  .then(
+    result => {
+      showSuccess('pilot mint tx complete',result)
+    },
+    error => {
+      showErrors(error.message)
+    }).
+  finally(() => {spinner.stopSpinner()});
+  
+  return txHash;
+}
+
+export async function upLevelPilot(pilotId) {
+  var gasPrice = await getGasPrice();
+  gasPrice = Math.trunc(gasPrice * 1.5);
+  
+  const accounts = await getAccounts();
+  if(accounts.length <= 0) {
+    throw new Error("connect to metamask");
+  }
+  
+  const estimatedGas = await citadelPilot.methods.upLevel(pilotId).estimateGas({from: accounts[0]});
+  
+  const tx = {
+    'from': accounts[0],
+    'to': CITADEL_PILOT,
+    'data': citadelPilot.methods.upLevel(pilotId).encodeABI(),
+    'gas': web3.utils.toHex(estimatedGas),
+    'gasPrice': web3.utils.toHex(gasPrice)
+  };
+  
+  spinner.startSpinner();
+  const txHash = await window.ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [tx],
+  })
+  .then(
+    result => {
+      showSuccess('pilot level-up tx complete',result)
+    },
+    error => {
+      showErrors(error.message)
+    }).
+  finally(() => {spinner.stopSpinner()});
+  
+  return txHash;
+}
+
+export async function buySovereignty(pilotId) {
+  var gasPrice = await getGasPrice();
+  gasPrice = Math.trunc(gasPrice * 1.5);
+  
+  const accounts = await getAccounts();
+  if(accounts.length <= 0) {
+    throw new Error("connect to metamask");
+  }
+  
+  const estimatedGas = await citadelPilot.methods.sovereignty(pilotId).estimateGas({from: accounts[0]});
+  
+  const tx = {
+    'from': accounts[0],
+    'to': CITADEL_PILOT,
+    'data': citadelPilot.methods.sovereignty(pilotId).encodeABI(),
+    'gas': web3.utils.toHex(estimatedGas),
+    'gasPrice': web3.utils.toHex(gasPrice)
+  };
+  
+  spinner.startSpinner();
+  const txHash = await window.ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [tx],
+  })
+  .then(
+    result => {
+      showSuccess('sovereignty tx complete',result)
+    },
+    error => {
+      showErrors(error.message)
+    }).
+  finally(() => {spinner.stopSpinner()});
+  
+  return txHash;
+}
+
+export async function checkPilot(pilotId) { // Does not require a connected wallet
+  const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTMetadata`;
+  const contractAddr = CITADEL_PILOT;
+  const tokenId = pilotId;
+  const tokenType = "erc721";
+
+  var config = {
+    method: 'get',
+    url: `${baseURL}?contractAddress=${contractAddr}&tokenId=${tokenId}&tokenType=${tokenType}`,
+    headers: { }
+  };
+
+  await axios(config)
+    .then(response => {
+      response.data.metadata.attributes.forEach(i => {showSuccess(`${i.trait_type.toString().toLowerCase()}: ${i.value.toString().toLowerCase()}`)});
+    })
+    .catch(error => console.log(error));
+
+  const pilot = await citadelPilot.methods.getOnchainPILOT(pilotId).call();
+  //add error handling
+
+  //TODO add cost to overthrow
+  if (pilot[0] ==  true) {
+    const sovereign = await citadelPilot.methods.getSovereign(pilotId).call();
+    showSuccess(`sovereign: ${pilot[0]}`);
+    showSuccess(`level: ${pilot[1]}`);
+    showSuccess(`kult: ${availKults[sovereign[2]]}`);
+    showSuccess(`kult bribes: ${sovereign[1]}`);
+  } else {
+    showSuccess(`sovereign: ${pilot[0]}`);
+    showSuccess(`level: ${pilot[1]}`);
+  }
+  
+  const owner = await citadelPilot.methods.ownerOf(pilotId).call()
+  .then( async (result) => {
+      showSuccess(`owner: ${result}`);  //  <<<< Make this a link to `check wallet`
+  },error => {
+    showErrors(error.message)
+  });
+
+  return;
+}
+
+export async function overthrowSovereign(sovereignId,pilotId) {
+  var gasPrice = await getGasPrice();
+  gasPrice = Math.trunc(gasPrice * 1.5);
+  
+  const accounts = await getAccounts();
+  if(accounts.length <= 0) {
+    throw new Error("connect to metamask");
+  }
+  
+  const estimatedGas = await citadelPilot.methods.overthrowSovereign(sovereignId,pilotId).estimateGas({from: accounts[0]});
+  
+  const tx = {
+    'from': accounts[0],
+    'to': CITADEL_PILOT,
+    'data': citadelPilot.methods.overthrowSovereign(sovereignId,pilotId).encodeABI(),
+    'gas': web3.utils.toHex(estimatedGas),
+    'gasPrice': web3.utils.toHex(gasPrice)
+  };
+  
+  spinner.startSpinner();
+  const txHash = await window.ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [tx],
+  })
+  .then(
+    result => {
+      showSuccess('overthrow tx complete',result)
+    },
+    error => {
+      showErrors(error.message)
+    }).
+  finally(() => {spinner.stopSpinner()});
+  
+  return txHash;
+}
+
+export async function bribeKult(sovereignId,kult) {
+  var gasPrice = await getGasPrice();
+  gasPrice = Math.trunc(gasPrice * 1.5);
+  
+  const accounts = await getAccounts();
+  if(accounts.length <= 0) {
+    throw new Error("connect to metamask");
+  }
+  
+  const estimatedGas = await citadelPilot.methods.bribeKult(sovereignId,kult).estimateGas({from: accounts[0]});
+  
+  const tx = {
+    'from': accounts[0],
+    'to': CITADEL_PILOT,
+    'data': citadelPilot.methods.bribeKult(sovereignId,kult).encodeABI(),
+    'gas': web3.utils.toHex(estimatedGas),
+    'gasPrice': web3.utils.toHex(gasPrice)
+  };
+  
+  spinner.startSpinner();
+  const txHash = await window.ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [tx],
+  })
+  .then(
+    result => {
+      showSuccess('bribe tx complete',result)
+    },
+    error => {
+      showErrors(error.message)
+    }).
+  finally(() => {spinner.stopSpinner()});
+  
+  return txHash;
+}
+
+//PILOT PUBLIC FUNCTIONS
+async function getSovereign(sovereignId) {
+  return (await citadelPilot.methods.getSovereign(sovereignId).call());
+}
+
+async function getSovereignCharge(sovereignId,chargeIndex) {
+  return (await citadelPilot.methods.getSovereignCharge(sovereignId,chargeIndex).call());
+}
+  
+async function getCitadelClaim(citadelId) {
+  return (await citadelPilot.methods.getCitadelClaim(citadelId).call());
+}
+  
+async function getOnchainPILOT(pilotId) {
+  return (await citadelExordium.methods.getOnchainPILOT(pilotId).call());
+}
